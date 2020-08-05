b5afaa2b3a16b716274c02ea8fdf6018d25e811a
Thomas de Graaff <t.de.graaff@vu.nl>
2020-08-05 15:53:15 +0200

    Update config.org??

--- config.el
+++ config.el
@@ -1,9 +1,23 @@
 ;;; config.el -*- lexical-binding: t; -*-
 
+(display-time-mode 1)                             ; Enable time in the mode-line
+
+(setq doom-theme 'doom-vibrant)
+(delq! t custom-theme-load-path)
+(custom-set-faces!
+  '(doom-modeline-buffer-modified :foreground "orange"))
+(defun doom-modeline-conditional-buffer-encoding ()
+  "We expect the encoding to be LF UTF-8, so only show the modeline when this is not the case"
+  (setq-local doom-modeline-buffer-encoding
+              (unless (or (eq buffer-file-coding-system 'utf-8-unix)
+                          (eq buffer-file-coding-system 'utf-8)))))
+
+(add-hook 'after-change-major-mode-hook #'doom-modeline-conditional-buffer-encoding)
+
 (setq
  doom-font (font-spec :family "Fira Mono" :size 16)
  doom-big-font (font-spec :family "Fira Mono" :size 36)
- doom-variable-pitch-font (font-spec :family "Overpass" :size 14))
+ doom-variable-pitch-font (font-spec :family "Overpass" :size 16))
 
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
